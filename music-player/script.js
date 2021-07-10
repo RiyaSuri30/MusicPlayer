8736c5667db3917866995eb99ef303b682ae7a0d
RiyaSuri30 <85056245+RiyaSuri30@users.noreply.github.com>
2021-07-10 22:36:25 +0530

    Files Added

---
+++ music-player/script.js
@@ -0,0 +1,114 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+//music
+const songs =
+[
+    {
+        name :'jacinto-1',
+        displayName:'Electric Chill Machine',
+        artist:'Jacinto Design',
+    },
+    {
+        name :'jacinto-2',
+        displayName:'Seven Nation Army (Remix)',
+        artist:'Jacinto Design',
+    },
+    {
+        name :'jacinto-3',
+        displayName:'Third Song',
+        artist:'Jacinto Design',
+    },
+    {
+        name :'metric-1',
+        displayName:'Fourth Song',
+        artist:'Jacinto Design',
+    }
+
+]
+
+
+
+//If Playing 
+let isPlaying = false;
+// Play
+function playSong()
+{
+    isPlaying=true;
+    playBtn.classList.replace('fa-play','fa-pause');
+    playBtn.setAttribute('title','Pause');
+    music.play();
+}
+// Pause
+function pauseSong()
+{
+    isPlaying= false;
+    playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+    music.pause();
+}
+
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click',()=>(isPlaying? pauseSong():playSong()));
+
+// Update Dom 
+function loadSong(song)
+{
+    title.textContent = song.displayName;
+    artist.textContent= song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+
+
+}
+// Current Song 
+let songIndex = 0;
+function prevSong()
+{
+    songIndex--;
+    if(songIndex < 0)
+    {
+        songIndex = songs.length-1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+function nextSong()
+{
+    songIndex++;
+    if(songIndex > songs.length - 1)
+    {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+// On start up - on load select first
+//loadSong(songs[songindex]);
+
+//Update Progress Bar & Time
+function updateProgressBar(e)
+{
+    if(isPlaying)
+    {
+       const {duration, currentTime} = e.srcElement;
+       console.log(duration,currentTime);
+       //Update Progress bar 
+       const progressPercent = (currentTime/duration)*100;
+       //console.log(progressPercent);
+       progress.style.width=`${progressPercent}%`;
+
+    }
+}
+
+// Event Listeners
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
+music.addEventListener('timeupdate',updateProgressBar);
